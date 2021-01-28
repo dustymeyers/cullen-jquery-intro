ea2182f7ad6495d862258083c2c6d48930121ce6
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-01-28 14:06:19 -0600

    added jquery, manipulated dom

--- myAwesomeScript.js
+++ myAwesomeScript.js
@@ -1,1 +1,22 @@
 console.log('I haz JS');
+
+$(document).ready(onReady);
+
+function onReady() {
+  console.log('onReady');
+  // $ is the name of jQuery's function
+  const h1 = $('h1');
+  console.log('h1', h1);
+
+  const h2 = $('h2');
+  // h2.css('background-color', 'green');
+  // same as $('h2').css('background-color', 'green
+  $('h2').addClass('froggy');
+
+  // change the text
+  const h1Text = $('h1').text();
+  console.log('h1Text is', h1Text);
+
+  const louderText = h1Text.toUpperCase() + '!!!!!';
+  $('h1').text(louderText);
+}
